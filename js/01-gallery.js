a699a56825a7c94a77aec8adba8d5dd13eb2a697
Ernes Nafieiev <demodekoz@gmail.com>
2022-11-10 22:12:35 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const createGalleryItems = (items) => {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    }).join("");
+}
+galleryEl.insertAdjacentHTML("beforeend", createGalleryItems(galleryItems));
